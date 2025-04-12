e05b8fadf99c0069b94963bdefce19f68708afcc
NIKHIL SHARMA <nikhilsharma101001@gmail.com>
2025-04-13 00:38:23 +0530

    wip: bug fixes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from "./generated/prisma";
 
-const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma };
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
