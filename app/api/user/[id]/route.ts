dbe174d1a07badf5f0e92b729accf9fce08d374b
NIKHIL SHARMA <nikhilsharma101001@gmail.com>
2025-05-02 17:54:51 +0530

    fix: deploy bugs

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -2,12 +2,6 @@ import prisma from "@/lib/prisma";
 import { verifyAuth } from "@/lib/server-service";
 import { NextRequest, NextResponse } from "next/server";
 
-interface Params {
-  params: {
-    id: string;
-  };
-}
-
 interface UserUpdateData {
   email?: string;
   name?: string;
@@ -16,7 +10,10 @@ interface UserUpdateData {
 }
 
 // PATCH - Update a specific user
-export async function PATCH(req: NextRequest, { params }: Params) {
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     // Verify authentication
     const auth = await verifyAuth(req);
@@ -24,8 +21,7 @@ export async function PATCH(req: NextRequest, { params }: Params) {
       return NextResponse.json({ error: auth.error }, { status: 401 });
     }
 
-    // Fix: Access id directly without awaiting params
-    const id = params.id;
+    const id = (await params).id;
 
     // Check if the user has admin role or is updating their own record
     if (auth.user?.role !== "admin" && auth.user?.id !== id) {
